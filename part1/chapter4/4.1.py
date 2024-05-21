4d2f90944eb0201f1b6eeb0a9fb2ce3aa6dfe5b7
Lixx <i@lixx.cn>
2024-05-21 18:07:50 +0800

    4

---
+++ part1/chapter4/4.1.py
@@ -0,0 +1,8 @@
+pizzas = ["pa", "pb", "pc"]
+for pizza in pizzas:
+    print(pizza)
+
+for pizza in pizzas:
+    print(f"I linke {pizza} pizza.")
+
+print("I really love pizza!")
---
+++ part1/chapter4/4.2.py
@@ -0,0 +1,4 @@
+pets = ["cat", "dog", "pig"]
+for pet in pets:
+    print(f"A {pet} would make a great pet.")
+print("Any of these animals would make a great pet!")
