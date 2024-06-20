086473f34bbc182d0fd60d0ddf5177a6846f292f
Lixx <i@lixx.cn>
2024-06-21 00:20:20 +0800

    project init

--- part1/chapter10/write_message.py
+++ part1/chapter10/write_message.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-path = Path('part1/chapter10/编程.txt')
+path = Path("part1/chapter10/编程.txt")
 
 # win
-# path.write_text("我喜欢编程。",encoding="utf-8") 
+# path.write_text("我喜欢编程。",encoding="utf-8")
 
 # linux
 contents = "我喜欢编程。\n"
 contents += "我喜欢创建"
-path.write_text("我喜欢编程。") 
+path.write_text("我喜欢编程。")
