1065c2a4e6d8f13b0bf1bffec7804fec9bb05d29
Lixx <i@lixx.cn>
2024-06-22 01:15:16 +0800

    alien invasion first commit

--- part1/chapter3/3.9.py
+++ part1/chapter3/3.9.py
@@ -1,4 +1,4 @@
-people = ['aa', 'bb', 'cc']
+people = ["aa", "bb", "cc"]
 print(people)
 print(people.pop(1))
 people.insert(1, "dd")
@@ -6,3 +6,6 @@ print(people)
 
 print("总共邀请了", len(people), "个人")
 print(people[-3])
+
+people.remove("cc")
+print(people)
--- part2_projects/alien_invasion/alien_invasion.py
+++ part2_projects/alien_invasion/alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 
 import pygame
+from bullet import Bullet
+from ship import Ship
 from settings import Settings
 
 
@@ -17,23 +19,61 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("外星人入侵")
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 侦听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # 每次循环时都重绘屏幕
-            # self.screen.fill(self.bg_color)
-            self.screen.fill(self.settings.bg_color)
-
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                elif event.key == pygame.K_q:
+                    sys.exit()
+                elif event.key == pygame.K_SPACE:
+                    self._fire_bullet()
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.ship.blitme()
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
